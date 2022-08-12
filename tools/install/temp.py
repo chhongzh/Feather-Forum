785e87bde121094927d9b1ae1a187b68703265c8
steven <2550519316@qq.com>
2022-08-12 10:35:10 +0800

    临时:临时文件

--- tools/install/temp.py
+++ tools/install/temp.py
@@ -8,4 +8,4 @@ with console.pager():
     console.print('')
     console.print('接下来需要您[b]手动[/b]完成!')
     console.print('')
-    console.print(f'打开{os.path.join(u,"dist")}')
+    console.print(f'1. 打开{os.path.join(u,"dist")}')
