d62a5001378b6197d055081c55589ed0fc2476d9
steven <2550519316@qq.com>
2022-08-12 10:34:58 +0800

    更新:整理代码

--- tools/build/buildFileTree.py
+++ tools/build/buildFileTree.py
@@ -10,20 +10,8 @@ current_address = os.path.dirname(os.path.abspath(__file__))
 obj = []
 
 
-def md5er(path):
-    m = hashlib.md5()  # 创建md5对象
-    with open(path, 'rb') as fobj:
-        while True:
-            data = fobj.read(1024)
-            if not data:
-                break
-            m.update(data)  # 更新md5对象
-
-    return m.hexdigest()  # 返回md5对象
-
-
 with console.status('扫描中...'):
-    console.log(os.getcwd())
+    console.log(f"当前目录:{os.getcwd()}")
     for parent, dirnames, filenames in os.walk('./'):
         if(os.path.basename(parent) == "node_modules" or os.path.basename(parent) == ".git" or os.path.basename(parent) == "dist"):
             dirnames[:] = []
@@ -41,3 +29,5 @@ with console.status('扫描中...'):
             obj.append(obj1)
 with open('./build.dist.json', 'w') as f:
     json.dump(obj, f, ensure_ascii=False)
+
+console.log('编译完成')
