20869c95a4bfd4d1fe3ec23ec512561bf936188f
steven <2550519316@qq.com>
2022-08-14 21:23:00 +0800

    更新:移除安装脚本,请使用git clone安装

--- tools/database/clear.sql
+++
@@ -1,9 +0,0 @@
--- Feather Forum 重置数据库
-
--- 删除POST
-DELETE FROM post;
-UPDATE sqlite_sequence SET seq = 0 WHERE name = 'post';
-
--- 删除用户
-DELETE FROM user;
-UPDATE sqlite_sequence SET seq = 0 WHERE name = 'user';
--- tools/install/README.md
+++
@@ -1,8 +0,0 @@
-安装工具
-===
-如果需要安装,请使用install.py
-```shell
-python3 install.py
-# or
-python install.py
-```
