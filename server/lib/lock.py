adce940cb43e133dc3f30f64728de349ebb16d3c
steven <2550519316@qq.com>
2022-08-16 20:09:20 +0800

    更新:优化锁

--- server/lib/lock.py
+++ server/lib/lock.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 class Lock(object):
     def __init__(self):
         self.lock = False
@@ -6,8 +9,8 @@ class Lock(object):
         self.lock = False
 
     def acquire(self):
-        while(self.lock):
-            pass
+        while (self.lock):
+            sleep(1)
         self.lock = True
         return True
 
