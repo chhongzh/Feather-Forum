d649f9542594e3351d5d6259ba7597c51313b58c
steven <2550519316@qq.com>
2022-08-16 13:10:34 +0800

    更新:增加管理员页面

---
+++ src/views/admin/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="4">
+                <el-card shadow="hover" header="用户总数">{{ userTotal }}</el-card>
+            </el-col>
+            <el-col :span="4">
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userTotal: 0
+        }
+    }
+}
+</script>
+
+<style>
+.--ff-box {
+    background-color: blue;
+    width: 100%;
+    height: 150px;
+    border-radius: 20px
+}
+</style>
---
+++ src/views/admin/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-row type="flex" justify="center">
+        <el-card header="管理员登录" shadow="hover" class="login-box">
+            <el-input size="large" v-model="n" placeholder="Username" clearable></el-input>
+            <div class="b"></div>
+            <el-input size="large" v-model="p" placeholder="Password" clearable></el-input>
+            <div class="b"></div>
+            <el-button size="large" type="primary">登录</el-button>
+        </el-card>
+    </el-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            n: '',
+            p: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+.b {
+    margin-top: 20px;
+}
+</style>
