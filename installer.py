fd2dc234aef517df1e63cd7af5f800ed43e4d2e9
steven <2550519316@qq.com>
2022-08-25 12:13:11 +0800

    优化:优化安装脚本

--- installer.py
+++ installer.py
@@ -1,117 +1,131 @@
+from os import system
 from sqlite3 import connect
+from pip import main as pip
+from time import sleep
+from threading import Thread, active_count
 
 
-def querry(title, q):
-    while (True):
-        print(title)
-        index = 1
-        print('Numeber\tSelect')
-        for i in q:
-            print(f'{index}\t{i}')
-            index += 1
-        val = input('Please Select:')
-        val: str
-        if (val.isdigit()):
-            val = int(val)-1
-            try:
-                q[val]
-                return val
-            except:
-                print('Unknow Input')
+class installThread(Thread):
+    def __init__(self, func, **kwargs):
+        Thread.__init__(self)
+        self.func = func
+        self.kwargs = kwargs
 
-        else:
-            print('Unknow Input')
+    def run(self):
+        self.func(**self.kwargs)
 
 
-def langs(path):
-    import json
-    with open(path) as f:
-        return json.load(f)
+print('欢迎使用全自动安装脚本')
+try:
+    input('按下任意键安装...(^C退出)')
+except:
+    print('取消操作!')
+    exit(0)
+tsk = [0, 0, 0, 0]
 
 
-print('Welcome to install Feather-Forum')
-print('')
-s = querry('Select a language before install.', ['简体中文', 'English'])
-lang = {}
-if (s == 0):
-    print('读取语言文件...')
-    lang = langs('install/lang/zhCn.json')
-elif (s == 1):
-    print('Read Lang File...')
-    lang = langs('install/lang/enUs.json')
-else:
-    print('未知语言(Unknow Lang)')
-    raise SystemError()
+def db():
+    global tsk
+    print('读取数据库...')
+    conn = connect('server/data.db3', check_same_thread=False)
+    c = conn.cursor()
+    c.executescript("""
 
-print(lang['resetDb']['ing'])
-conn = connect('server/data.db3', check_same_thread=False)
-c = conn.cursor()
-c.executescript("""
+    PRAGMA foreign_keys = false;
 
-PRAGMA foreign_keys = false;
+    -- ----------------------------
+    -- Table structure for post
+    -- ----------------------------
+    DROP TABLE IF EXISTS "post";
+    CREATE TABLE "post" (
+    "title" TEXT,
+    "content" TEXT,
+    "uid" INTEGER,
+    "time" DATE,
+    "pid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    );
 
--- ----------------------------
--- Table structure for post
--- ----------------------------
-DROP TABLE IF EXISTS "post";
-CREATE TABLE "post" (
-  "title" TEXT,
-  "content" TEXT,
-  "uid" INTEGER,
-  "time" DATE,
-  "pid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
-);
+    -- ----------------------------
+    -- Table structure for superuser
+    -- ----------------------------
+    DROP TABLE IF EXISTS "superuser";
+    CREATE TABLE "superuser" (
+    "name" TEXT,
+    "pw" TEXT,
+    "authkey" TEXT,
+    "last" INTEGER,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    );
 
--- ----------------------------
--- Table structure for superuser
--- ----------------------------
-DROP TABLE IF EXISTS "superuser";
-CREATE TABLE "superuser" (
-  "name" TEXT,
-  "pw" TEXT,
-  "authkey" TEXT,
-  "last" INTEGER,
-  "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
-);
+    -- ----------------------------
+    -- Table structure for user
+    -- ----------------------------
+    DROP TABLE IF EXISTS "user";
+    CREATE TABLE "user" (
+    "name" TEXT,
+    "password" TEXT,
+    "last" integer DEFAULT 0,
+    "time" integer,
+    "uid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uuid" text,
+    "avartar" TEXT DEFAULT 什么都没留下,
+    "coin" integer DEFAULT 0,
+    "email" TEXT,
+    "authkey" TEXT
+    );
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
-DROP TABLE IF EXISTS "user";
-CREATE TABLE "user" (
-  "name" TEXT,
-  "password" TEXT,
-  "last" integer DEFAULT 0,
-  "time" integer,
-  "uid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "uuid" text,
-  "avartar" TEXT DEFAULT 什么都没留下,
-  "coin" integer DEFAULT 0,
-  "email" TEXT,
-  "authkey" TEXT
-);
+    -- ----------------------------
+    -- Table structure for config
+    -- ----------------------------
+    DROP TABLE IF EXISTS "config";
+    CREATE TABLE "config" (
+    "value" TEXT,
+    "key" TEXT
+    );
 
--- ----------------------------
--- Table structure for config
--- ----------------------------
-DROP TABLE IF EXISTS "config";
-CREATE TABLE "config" (
-  "value" TEXT,
-  "key" TEXT
-);
+    -- ----------------------------
+    -- Records of config
+    -- ----------------------------
+    BEGIN;
+    INSERT INTO "config" ("value", "key") VALUES ('1800', 'authKeyTime');
+    INSERT INTO "config" ("value", "key") VALUES ('5', 'itemLimit');
+    COMMIT;
 
--- ----------------------------
--- Records of config
--- ----------------------------
-BEGIN;
-INSERT INTO "config" ("value", "key") VALUES ('1800', 'authKeyTime');
-INSERT INTO "config" ("value", "key") VALUES ('5', 'itemLimit');
-COMMIT;
-
-PRAGMA foreign_keys = true;
+    PRAGMA foreign_keys = true;
 
 
-""")
-conn.commit()
-conn.close()
-print(lang['resetDb']['finish'])
+    """)
+    conn.commit()
+    conn.close()
+    print('写入数据库成功!')
+    tsk[0] = 1
+
+
+def lib():
+    global tsk
+    print('安装依赖...')
+    pip(['install', 'rich', 'flask'])
+    print('安装完成!')
+    tsk[1] = 1
+
+
+def npminstall():
+    global tsk
+    print('安装npm依赖!')
+    system('npm install')
+    print('安装npm依赖完成!')
+    tsk[2] = 0
+
+
+def npmbuild():
+    global tsk
+    print('编译Npm...')
+    system('npm run build')
+    print('编译完成!')
+    tsk[3] = 0
+
+
+db()
+lib()
+npminstall()
+npmbuild()
