9457923835753ac171a4145967842e8bae9ba5a9
steven <2550519316@qq.com>
2022-08-31 14:15:02 +0800

    更新:全新的安装界面

--- installer.py
+++ installer.py
@@ -1,32 +1,128 @@
-from os import system
+from json import dumps
+import random
+import socket
 from sqlite3 import connect
-from pip import main as pip
-from time import sleep
-from threading import Thread, active_count
-
-
-class installThread(Thread):
-    def __init__(self, func, **kwargs):
-        Thread.__init__(self)
-        self.func = func
-        self.kwargs = kwargs
-
-    def run(self):
-        self.func(**self.kwargs)
-
-
-print('欢迎使用全自动安装脚本')
+from pip import main
 try:
-    input('按下任意键安装...(^C退出)')
-except:
-    print('取消操作!')
+    from flask import Flask, render_template, request
+    from flask_socketio import SocketIO
+except ImportError:
+    print('安装Flask')
+    main(['install', 'flask', 'flask-socketio'])
+    print('完成')
+    print('请重启脚本!')
     exit(0)
-tsk = [0, 0, 0, 0]
+import os
+import platform
+import sys
 
 
-def db():
-    global tsk
-    print('读取数据库...')
+print('启动Web服务器')
+app = Flask(__name__, template_folder='install', static_folder='install')
+io = SocketIO(app)
+
+
+def extract_ip():
+    st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        st.connect(('10.255.255.255', 1))
+        IP = st.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        st.close()
+    return IP
+
+
+def randstr(num):
+    H = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    salt = ''
+    for _ in range(num):
+        salt += random.choice(H)
+
+    return salt
+
+
+def request_parse(req_data):
+    if (req_data.method == 'POST'):
+        data = req_data.json
+    elif (req_data.method == 'GET'):
+        data = req_data.args
+    return data
+
+
+key = randstr(16)
+
+print('请访问:http://{}:5000'.format(extract_ip()))
+
+
+@app.route('/')
+def index():
+    error = False
+    obj = []
+    obj1 = {}
+
+    for root, dirs, files in os.walk("."):
+        dirs[:] = [d for d in dirs if d not in ["node_modules", ".git"]]
+
+        for name in files:
+            obj1['name'] = os.path.join(root, name)
+            obj1['need'] = True
+            obj1['current'] = os.access(
+                obj1['name'], os.R_OK) and os.access(obj1['name'], os.W_OK)
+            if (not obj1['current']):
+                error = True
+            obj.append(obj1.copy())
+            obj1.clear()
+
+    vererror = not (sys.version_info.major, sys.version_info.minor,
+                    sys.version_info.micro) >= (3, 9, 6)
+    return render_template('step1.html',
+                           UseSystem=platform.system(),
+                           UsePython="{}.{}.{}".format(
+                               sys.version_info.major, sys.version_info.minor, sys.version_info.micro),
+                           items=obj,
+                           HasError=error,
+                           Key=key,
+                           PyVer=(sys.version_info.major,
+                                  sys.version_info.minor, sys.version_info.micro),
+                           PyErr=vererror)
+
+
+@app.route('/step2', methods=['GET'])
+def step2():
+    return render_template('step2.html')
+
+
+@io.on('connect')
+def wsconnect():
+    print('Ws Client Was Connect')
+
+
+@io.on('disconnect')
+def wsdisconnect():
+    print('Ws Client Was Disconnect')
+
+
+@io.on('install run')
+def installrun(data):
+    data = data['data']
+    obj = {}
+    obj['forumName'] = data['name']
+    obj['baseURL'] = data['api']
+    obj['webhost'] = 'auto'
+    obj['hideFeather'] = False
+    obj = dumps(obj, ensure_ascii=False)
+    with open('src/assets/js/config.js', 'w') as f:
+        f.write("export default "+obj+";")
+    io.emit('server log', '将配置写入文件:'+'src/assets/js/config.js')
+    io.emit('server log', '准备安装依赖')
+    for i in ['flask', 'flask-socketio', 'rich', 'flask-cors']:
+        io.emit('server log', f'执行:安装 {i}')
+        main(['install', i])
+        io.emit('server log', f'执行:完成 {i} 的安装')
+    io.emit('server log', '执行:初始化数据库')
     conn = connect('server/data.db3', check_same_thread=False)
     c = conn.cursor()
     c.executescript("""
@@ -97,35 +193,16 @@ def db():
     """)
     conn.commit()
     conn.close()
-    print('写入数据库成功!')
-    tsk[0] = 1
+    io.emit('server log', '执行:数据库清理完成')
+    io.emit('install done', '完成安装!')
 
 
-def lib():
-    global tsk
-    print('安装依赖...')
-    pip(['install', 'rich', 'flask'])
-    print('安装完成!')
-    tsk[1] = 1
+@io.on('server stop')
+def stop():
+    import os
+    import signal
+    os.kill(os.getpid(), signal.SIGKILL)
 
 
-def npminstall():
-    global tsk
-    print('安装npm依赖!')
-    system('npm install')
-    print('安装npm依赖完成!')
-    tsk[2] = 0
-
-
-def npmbuild():
-    global tsk
-    print('编译Npm...')
-    system('npm run build')
-    print('编译完成!')
-    tsk[3] = 0
-
-
-db()
-lib()
-npminstall()
-npmbuild()
+# app.run()
+io.run(app, host="0.0.0.0", debug=True)
