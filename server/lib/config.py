a7a3a6cc5a625e6df4dc7cb9e31f3c2d5e9476c4
steven <2550519316@qq.com>
2022-09-02 08:40:29 +0800

    版本1.23.3

--- server/lib/config.py
+++ server/lib/config.py
@@ -2,6 +2,6 @@
 class Config(object):
     UseDebugMode = True
     ServerPort = 14524
-    Version = [1, 23, 1]
+    Version = [1, 23, 2]
     SecretKey = 'Feather-Forum Demo on Debug Mode !'
 # ---------------------------------------------------------------------------
