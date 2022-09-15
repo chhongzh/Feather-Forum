ab09be77883415106e6b6ebcc7d77a606730b36f
steven <2550519316@qq.com>
2022-09-15 10:47:10 +0800

    杂项:移除不需要的依赖

--- src/main.js
+++ src/main.js
@@ -7,14 +7,6 @@ import VueAxios from "vue-axios";
 import Axios from "axios";
 import NProgress from "nprogress";
 import config from "./assets/js/config";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faCircleUser,
-  faClipboard,
-  faUser,
-  faClock,
-} from "@fortawesome/free-regular-svg-icons";
 
 import "./assets/js/prism.js";
 
@@ -27,32 +19,22 @@ import VMdEditor from "@kangc/v-md-editor/lib/codemirror-editor";
 import "@kangc/v-md-editor/lib/style/codemirror-editor.css";
 import githubTheme from "@kangc/v-md-editor/lib/theme/github.js";
 import "@kangc/v-md-editor/lib/theme/style/github.css";
-// import vuepressTheme from "@kangc/v-md-editor/lib/theme/vuepress.js";
-// import "@kangc/v-md-editor/lib/theme/style/vuepress.css";
 
-// highlightjs
 import hljs from "highlight.js";
 
-// codemirror 编辑器的相关资源
 import Codemirror from "codemirror";
-// mode
 import "codemirror/mode/markdown/markdown";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import "codemirror/mode/htmlmixed/htmlmixed";
 import "codemirror/mode/vue/vue";
-// edit
 import "codemirror/addon/edit/closebrackets";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/matchbrackets";
-// placeholder
 import "codemirror/addon/display/placeholder";
-// active-line
 import "codemirror/addon/selection/active-line";
-// scrollbar
 import "codemirror/addon/scroll/simplescrollbars";
 import "codemirror/addon/scroll/simplescrollbars.css";
-// style
 import "codemirror/lib/codemirror.css";
 import createTipPlugin from "@kangc/v-md-editor/lib/plugins/tip/index";
 import "@kangc/v-md-editor/lib/plugins/tip/tip.css";
@@ -88,13 +70,10 @@ router.afterEach(() => {
   NProgress.done();
 });
 
-library.add(faCircleUser, faClipboard, faUser, faClock);
-
 app.use(store);
 app.use(router);
 app.use(ElementPlus);
 app.use(VueAxios, Axios);
-app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(VMdEditor);
 
 app.mount("#app");
