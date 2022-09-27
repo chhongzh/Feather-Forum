cb6ae880681f07e4e3f97d269b273c5c34128f9b
steven <2550519316@qq.com>
2022-09-27 08:29:00 +0800

    功能:安装器增加重置功能

--- installer.py
+++ installer.py
@@ -84,6 +84,11 @@ def step2():
     return render_template('step2.html', BuildVer=data['version'])
 
 
+@app.route('/reset')
+def reset():
+    return render_template('reset.html', BuildVer=data['version'])
+
+
 @io.on('connect')
 def wsconnect():
     print('Ws Client Was Connect')
@@ -184,9 +189,7 @@ def installrun(data):
     CREATE TABLE "follow" (
     "from" INTEGER NOT NULL,
     "to" INTEGER NOT NULL,
-    "time" INTEGER,
-    "fromcache" TEXT NOT NULL,
-    "tocache" TEXT NOT NULL
+    "time" INTEGER
     );
     PRAGMA foreign_keys = true;
     """)
@@ -201,6 +204,91 @@ def installrun(data):
     io.emit('server log', '执行:yarn build完成')
 
     io.emit('install done', '完成安装!')
+
+
+@io.on('reset start')
+def resetall():
+    io.emit('server log', '执行:重置数据库')
+    conn = connect('server/data.db3', check_same_thread=False)
+    c = conn.cursor()
+    c.executescript("""
+
+    PRAGMA foreign_keys = false;
+
+    -- ----------------------------
+    -- Table structure for post
+    -- ----------------------------
+    DROP TABLE IF EXISTS "post";
+    CREATE TABLE "post" (
+    "title" TEXT,
+    "content" TEXT,
+    "uid" INTEGER,
+    "time" DATE,
+    "pid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    );
+
+    -- ----------------------------
+    -- Table structure for superuser
+    -- ----------------------------
+    DROP TABLE IF EXISTS "superuser";
+    CREATE TABLE "superuser" (
+    "name" TEXT,
+    "pw" TEXT,
+    "authkey" TEXT,
+    "last" INTEGER,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+    );
+
+    -- ----------------------------
+    -- Table structure for user
+    -- ----------------------------
+    DROP TABLE IF EXISTS "user";
+    CREATE TABLE "user" (
+    "name" TEXT,
+    "password" TEXT,
+    "last" integer DEFAULT 0,
+    "time" integer,
+    "uid" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "uuid" text,
+    "avartar" TEXT DEFAULT 什么都没留下,
+    "coin" integer DEFAULT 0,
+    "email" TEXT,
+    "authkey" TEXT
+    );
+
+    -- ----------------------------
+    -- Table structure for config
+    -- ----------------------------
+    DROP TABLE IF EXISTS "config";
+    CREATE TABLE "config" (
+    "value" TEXT,
+    "key" TEXT
+    );
+
+    -- ----------------------------
+    -- Records of config
+    -- ----------------------------
+    BEGIN;
+    INSERT INTO "config" ("value", "key") VALUES ('1800', 'authKeyTime');
+    INSERT INTO "config" ("value", "key") VALUES ('5', 'itemLimit');
+    COMMIT;
+
+
+    -- ----------------------------
+    -- Table structure for follow
+    -- ----------------------------
+    DROP TABLE IF EXISTS "follow";
+    CREATE TABLE "follow" (
+    "from" INTEGER NOT NULL,
+    "to" INTEGER NOT NULL,
+    "time" INTEGER
+    );
+    PRAGMA foreign_keys = true;
+    """)
+    conn.commit()
+    conn.close()
+    io.emit('server log', '执行:重置数据库完成')
+    io.emit('reset done')
 
 
 @io.on('server stop')
