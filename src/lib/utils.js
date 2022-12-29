3826483eb956e6213819c8b8fb9c2abc523656d5
steven <2550519316@qq.com>
2022-12-29 11:41:45 +0800

    功能:Notification实现

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,5 @@
 import { useTitle } from "@vueuse/core";
+import { ElNotification } from "element-plus";
 /**
  * 设置题目
  * @param {string} title 题目
@@ -7,4 +8,17 @@ const setWindowTitle = (title) => {
   useTitle(title);
 };
 
-export { setWindowTitle };
+/**
+ * 打开一个Notification
+ * @param {string} title 需要弹出的题目
+ * @param {string} content 需要显示的内容
+ */
+const makeNotification = (title, content) => {
+  ElNotification({
+    title: title,
+    message: content,
+    position: "bottom-left",
+  });
+};
+
+export { setWindowTitle, makeNotification };
