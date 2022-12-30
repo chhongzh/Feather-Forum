f698b24f1778a17955c3f615ca80b8ba6b667662
steven <2550519316@qq.com>
2022-12-30 14:52:27 +0800

    功能:增加一些功能

--- src/views/MyView.vue
+++ src/views/MyView.vue
@@ -1,10 +1,25 @@
 <template>
-    <el-card>
-        <template #header>
-            <h1>{{ uname }}</h1>
-        </template>
-        <el-button @click="logout" type="primary">{{ $t('message.logout') }}</el-button>
-    </el-card>
+    <el-row type="flex" justify="center">
+        <el-card style="width:80%">
+            <template #header>
+                <h1>{{ uname }}</h1>
+            </template>
+            <el-tabs tab-position="left" style="" class="demo-tabs">
+                <el-tab-pane label="常规">
+                    <el-button @click="logout" type="primary">{{ $t('message.logout') }}</el-button>
+                </el-tab-pane>
+                <el-tab-pane label="账户">Config</el-tab-pane>
+                <el-tab-pane label="消息">
+                    <div v-if="renderNotification">
+                        <p>我们检查到您的浏览器拒绝了消息请求, 因此您可能无法及时收到消息</p>
+                        <p>也有可能是因为检测失误, 您可以点击下方按钮进行测试</p>
+                        <el-button :disabled="testDisabled" @click="testNotification">{{ test }}</el-button>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="更多"></el-tab-pane>
+            </el-tabs>
+        </el-card>
+    </el-row>
 </template>
 
 <script>
@@ -14,7 +29,10 @@ import { delLocalAuthkey } from '@/lib/auth'
 export default {
     data() {
         return {
-            uname: ''
+            uname: '',
+            renderNotification: true,
+            test: "测试",
+            testDisabled: false
         }
     },
     mounted() {
@@ -36,6 +54,17 @@ export default {
             this.$store.commit('logout')
             this.$router.push('/')
         }
+
+        if (Notification.permission == "default") {
+            this.test = "您还未同意请求"
+            this.testDisabled = false
+        } else if (Notification.permission == "denied") {
+            this.test = "您拒绝了请求"
+            this.testDisabled = true
+        } else {
+            this.test = "您同意了请求"
+            this.testDisabled = true
+        }
     },
     methods: {
         logout() {
@@ -44,6 +73,20 @@ export default {
             this.$store.commit('name', '')
             this.$store.commit('logout')
             this.$router.push('/login')
+        },
+        testNotification() {
+            this.test = "测试中..."
+            this.testDisabled = true
+            Notification.requestPermission((permissions) => {
+                if (permissions == "granted") {
+                    this.test = "您同意了请求"
+                } else if (permissions == "denied") {
+                    this.test = "您拒绝了请求"
+                } else {
+                    this.test = "您还未同意请求"
+                    this.testDisabled = false
+                }
+            })
         }
     }
 }
